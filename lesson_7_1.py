2bb371d91c538be5035af41e1993d3b3a428c43b
Evgeny Abdulin <evgenyabdulin@Evgenys-Mac-mini.local>
2021-05-03 17:29:50 -0500

    lesson_7_1.py added

---
+++ lesson_7_1.py
@@ -0,0 +1,20 @@
+import random
+
+random_number = random.randrange(50)
+# print(random_number)
+
+attempts = 7
+
+while attempts > 0:
+    print("Attempts left:" + str(attempts))
+    attempts -= 1
+    guessed_number = int(input("Guess the number?"))
+    if guessed_number == random_number:
+        print ("You guessed correctly")
+        attempts = 0
+    elif guessed_number > random_number:
+        print ("The number you gave is too low")
+    else:
+        print("The number you gave is too high")
+
+
